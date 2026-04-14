5d75e0a46668811f1152c2680f8e923a8daef785
adielazari <adiel@ionix.io>
2026-04-15 02:00:43 +0300

    feat: install better-sqlite3, configure Next.js for SQLite

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {},
+  experimental: {
+    serverComponentsExternalPackages: ["better-sqlite3"],
+  },
 };
 
 module.exports = nextConfig;
